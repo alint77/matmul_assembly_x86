52279ed7a3cbd1f9802b3aa4211f435163e0c065
AliNT <ali.nt1377@gmail.com>
2025-02-02 12:22:15 +0000

    matmul_scalar_1 works correctly

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Implementing float32 Matrix Multiplication in X86_64 assembly and attempting to optimise using various techniques.  
+Implementing float32 Matrix Multiplication in X86_64 assembly and attempting to optimise using various techniques.
+
+
+matmul_scalar_1.asm : Naive implementation.
--- matmul_scalar_1.asm
+++ matmul_scalar_1.asm
@@ -1,23 +1,23 @@
 %include "header.inc"
+global main
+extern printf
 
 section .data
-    a_matrix_rmaj: dd 2.0,3.0,4.0 , 5.0,6.0,7.0 , 8.0,9.0,10.0 , 11.0,12.0,13.0 , 14.0,15.0,16.0
+    a_matrix_rmaj: dd 2.1,3.1,4.1 , 5.1,6.1,7.1 , 8.1,9.1,10.1 , 11.1,12.1,13.1 , 14.1,15.1,16.1
     a_matrix_rows: dq 5
     a_matrix_cols: dq 3
-    b_matrix_rmaj: dd 2.0,3.0,4.0,5.0 , 2.0,3.0,4.0,5.0 , 2.0,3.0,4.0,5.0
+    b_matrix_rmaj: dd 2.1,3.1,4.1,5.1 , 2.1,3.1,4.1,5.1 , 2.1,3.1,4.1,5.1
     b_matrix_rows: dq 3
     b_matrix_cols: dq 4
 
+    fmt: db "%f", 0x0a, 0
 
 section .bss
-    num: resb 9 
     c_matrix_rmaj: resd 20
 
-section .text
-global _start
 
-_start:
-xor rbx,rbx
+section .text
+main:
 xor r10,r10 ; i
 .loop_a_rows:
 xor r11,r11 ; j
@@ -25,9 +25,6 @@ xor r11,r11 ; j
     xor r12,r12 ; k
     xorps xmm2,xmm2 ; accumulator
     .loop_dotprod:
-
-        ; logic
-        inc rbx
         
         mov r14,r10; r14 used for mat1 idx
         imul r14,[a_matrix_cols]
@@ -36,7 +33,7 @@ xor r11,r11 ; j
         movss xmm0,  [a_matrix_rmaj+r14*4]    ;  xmm0 used for a[i][k]
         
         mov r8,r12 ; r8 holds k
-        imul r8,[b_matrix_rows]
+        imul r8,[b_matrix_cols]
         add r8,r11; r8 = [k*b_rows + j] = [k][j]
         
         movss xmm1,  [b_matrix_rmaj+r8*4]    ;  xmm1 used for b[k][j]
@@ -45,17 +42,31 @@ xor r11,r11 ; j
 
         addss xmm2,xmm0 ; xmm2 hold the dotprod so far
 
+
         inc r12
         cmp r12,[a_matrix_cols] ; loop control
         jl .loop_dotprod
 
-        ; TODO: mov sum to mat_C [i][j]
+
+        ; calling printf to display the result inside xmm2 
+        cvtss2sd xmm0, xmm2 
+        lea rdi, [rel fmt] ; address of label
+        mov rax, 1       ; AL=1
+        push rbx
+        push r11
+        push r10
+        call printf
+        pop r10
+        pop r11
+        pop rbx
+
         ; Save dotprod into c_matrix_rmaj[i][j]
         mov rax, r10              ; rax = i
         imul rax, [b_matrix_cols] ; rax = i * b_matrix_cols
         add rax, r11              ; rax = (i * b_matrix_cols) + j
         shl rax, 2                ; multiply by 4 for float32
-        movss [c_matrix_rmaj + rax], xmm2 ; store dot product
+        movss [c_matrix_rmaj + rax*4], xmm2 ; store dot product
+    
 
     inc r11
     cmp r11,[b_matrix_cols] ; loop control
@@ -65,20 +76,6 @@ inc r10
 cmp r10,[a_matrix_rows] ; loop control
 jl .loop_a_rows
 
-mov rcx,rbx
-call _print_rcx
 
 exit 0
 
-
-_print_rcx:
-    add rcx,48
-    mov [num],rcx
-    mov rcx,10
-    mov [num+8],rcx
-    mov rax,SYS_WRITE
-    mov rdi,STDOUT
-    mov rsi,num
-    mov rdx,9
-    syscall 
-    ret
